6cd4b7a2e0ae6d75d388207b0566081a26507f6c
BobErgot <25260745+BobErgot@users.noreply.github.com>
2022-11-15 18:45:26 -0500

    Add base root templates for app, index and theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import '@mui/material';
+import 'react-icons';
+import 'react-icons/bi';
+import 'react-icons/md';
+import 'react-icons/bs';
+import 'react-router-dom';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
+  initiateSocketConnection();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <CssBaseline />
+        <Routes>
+ 
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
