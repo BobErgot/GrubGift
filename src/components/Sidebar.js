144d1490c3a0e80cd23663abc1bd5534f199ba4f
BobErgot <25260745+BobErgot@users.noreply.github.com>
2022-11-16 20:43:12 -0500

    Add sidebar, footer, loading, copyright components & user api call

---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import {Stack} from "@mui/material";
+import React from "react";
+import FindUsers from "./FindUsers";
+import Footer from "./Footer";
+
+const Sidebar = () => {
+    return (
+        <Stack spacing={2}>
+            <FindUsers/>
+            <Footer/>
+        </Stack>
+    );
+};
+
+export default Sidebar;
--- src/views/ExploreView.js
+++ src/views/ExploreView.js
@@ -2,12 +2,13 @@ import {Container} from "@mui/material";
 import React from "react";
 import GridLayout from "../GridLayout";
 import Navbar from "../components/Navbar";
-
+import Sidebar from "../components/Sidebar";
 const ExploreView = () => {
     return (
         <Container>
             <Navbar/>
             <GridLayout
+                right={<Sidebar/>}
             />
         </Container>
     );
